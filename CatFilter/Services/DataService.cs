be550be3d37b541e3c0c6801059ea84ef114b11d
Vasiliy Uchanev <Vasily@sharp-dev.net>
2016-07-25 16:24:47 +0500

    DataService

---
+++ CatFilter/Services/DataService.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MvvmCross.Plugins.Sqlite;
+using SQLite.Net;
+
+namespace CatFilter.Core.Services
+{
+	public class DataService : IDataService
+	{
+		private readonly SQLiteConnection _connection;
+
+		public DataService(IMvxSqliteConnectionFactory factory)
+		{
+			_connection = factory.GetConnection("cat.sql");
+			_connection.CreateTable<Kitten>();
+		}
+
+		public List<Kitten> KittensMatching(string filter)
+		{
+			return _connection.Table<Kitten>()
+				.Where(c => c.Name.Contains(filter))
+				.OrderBy(c => c.Price)
+				.ToList();
+		}
+
+		public void Insert(Kitten kitten)
+		{
+			_connection.Insert(kitten);
+		}
+
+		public void Update(Kitten kitten)
+		{
+			_connection.Update(kitten);
+		}
+
+		public void Delete(Kitten kitten)
+		{
+			_connection.Delete(kitten);
+		}
+
+		public int Count => _connection.Table<Kitten>().Count();
+	}
+}
---
+++ CatFilter/Services/IDataService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CatFilter.Core.Services
+{
+	public interface IDataService
+	{
+		List<Kitten> KittensMatching(string filter);
+		void Insert(Kitten kitten);
+		void Update(Kitten kitten);
+		void Delete(Kitten kitten);
+		int Count { get; }
+	}
+}
